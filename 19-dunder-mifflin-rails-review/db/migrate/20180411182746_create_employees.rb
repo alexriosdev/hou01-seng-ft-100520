eb2f7533d20c49f62493231c66258cc8a940cefc
Shivang Dave <daveshivang15@gmail.com>
2020-11-03 11:36:41 -0600

    Add review code..

---
+++ 19-dunder-mifflin-rails-review/db/migrate/20180411182746_create_employees.rb
@@ -0,0 +1,16 @@
+class CreateEmployees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :alias
+      t.string :title
+      t.string :office
+      t.string :img_url
+      # t.integer :dog_id
+      t.references :dog
+
+      t.timestamps
+    end
+  end
+end
